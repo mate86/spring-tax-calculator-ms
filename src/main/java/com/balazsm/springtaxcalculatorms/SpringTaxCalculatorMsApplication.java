118d5b13734b583a61460c5a5ed901fbf6880b06
Máté Balázs <mate.balazs@sdu.blackbelt.hu>
2022-09-26 14:29:50 +0200

    Initial commit

---
+++ src/main/java/com/balazsm/springtaxcalculatorms/SpringTaxCalculatorMsApplication.java
@@ -0,0 +1,13 @@
+package com.balazsm.springtaxcalculatorms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaxCalculatorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaxCalculatorMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balazsm/springtaxcalculatorms/SpringTaxCalculatorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balazsm.springtaxcalculatorms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTaxCalculatorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
